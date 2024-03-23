e0ad29df601918ca2e0548c54097e3e532305fac
Eric Chou <yphbchou0911@gmail.com>
2024-03-24 01:30:49 +0800

    done!

--- ds_hw2/hw2.cpp
+++ ds_hw2/hw2.cpp
@@ -1,19 +1,15 @@
 #include <iostream>
 #include <fstream>
-#include <sstream>
-#include <string>
+
 #define STACK_SIZE 4
 #define QUEUE_SIZE 8
 #define MAX_WEIGHT 260
 
 using namespace std;
-
-class Queue 
-{ // Queue class
+class Queue { // Queue class
 private:
     int front, rear, capacity;
     int* queue;
-
 public:
     Queue(int size) 
     {
@@ -21,32 +17,28 @@ public:
         capacity = size;
         queue = new int[capacity];
     }
-
     bool isEmpty() 
     {
         return front == rear;
     }
-
     bool isFull(){
         return (rear+1) % capacity == front;
     }
-    void push(int weight) {
+    void push(int weight) 
+    {
         queue[rear] = weight;
         rear = (rear+1) % capacity;
     }
-
-    int pop() {
+    int pop() 
+    {
         int weight = queue[front];
         front = (front + 1) % capacity;
         return weight;
     }
-    int size() {
+    int size() 
+    {
         return (rear - front + capacity) % capacity;
     }
-    int next(){
-        return queue[front];
-    }
-
 
 };
 
@@ -54,39 +46,39 @@ class Stack {  // Stack class
 private:
     int top, capacity;
     int* stack;
-
 public:
-    Stack(int size) {
+    Stack(int size) 
+    {
         top = -1;
         capacity = size;
         stack = new int[capacity];
     }
-
-    void push(int weight) {
+    void push(int weight) 
+    {
         if (isFull()) {
             cout << "Stack is full!" << endl;
             return;
         }
         stack[++top] = weight;
     }
-
-    int pop() {
+    int pop() 
+    {
         if (isEmpty()) {
             cout << "Stack is empty!" << endl;
             return -1;
         }
         return stack[top--];
     }
-
-    bool isEmpty() {
+    bool isEmpty() 
+    {
         return top == -1;
     }
-
-    bool isFull() {
+    bool isFull() 
+    {
         return top == capacity - 1;
     }
-
-    int sum() {
+    int sum() 
+    {
         int total = 0;
         for (int i = 0; i <= top; ++i) {
             total += stack[i];
@@ -95,95 +87,138 @@ public:
     }
 };
 
+void student_to_station(Queue& train_station, Queue& student_weight);
+void station_to_bus(Queue& train_station, Stack& bus);
+void bus_to_eebuilding(Stack& bus, Queue& eebuilding);
+void eebuilding_print(Queue& eebuilding, ofstream& outputFile);
+
 int main(){
     ifstream inputFile("input.txt");
     ofstream outputFile("output.txt");
-    if (!inputFile.is_open()) {
-        cout << "Failed to open input file." << endl;
-        return 1;
-    }
 
+    Queue train_station  (QUEUE_SIZE+1);
+    Stack bus            (STACK_SIZE);
+    
     int test_case; //測資數量
     inputFile >> test_case;
-
     for (int i = 0; i < test_case; ++i) 
     {
         int student_num; //學生數量
         inputFile >> student_num;
-        
         Queue student_weight (student_num+1);
-        Queue eebuilding (student_num+1);
-        Queue train_station(QUEUE_SIZE+1);
-        Stack bus(STACK_SIZE);
-        //70,67,75,55,51,40,87,65,58,86,66,43,57,76,68,61,60,63,39,74
+        Queue eebuilding     (student_num+1);
+
+        if(student_num < 8 || student_num>100) 
+        {   //題目人數限制
+            outputFile << "student number should be between 8 and 100 !"<<endl;
+            for( int j = 0 ; j < student_num ; j++ )
+            {
+                int weight ;
+                inputFile >> weight;         
+                student_weight.push(weight);
+                if (j!=student_num-1)
+                {        
+                    inputFile.ignore(1, ',');
+                }
+            }
+            inputFile.clear();
+            continue;
+        }
+
+        bool valid_weight = true;
         for( int j = 0 ; j < student_num ; j++ )
         {
             int weight ;
-            char comma ;
-            inputFile >> weight;  //讀取學生體重
-            student_weight.push(weight);
-            if(j!=student_num-1){   //忽略逗號
-                inputFile >> comma;
+            inputFile >> weight;         
+            if (weight<0 || weight > 260)
+            {   //題目體重限制
+                valid_weight = false;
             }
+            student_weight.push(weight);
+            if (j!=student_num-1)
+            {        //忽略逗號
+                inputFile.ignore(1, ',');
+            }
+        }
+
+        if(!valid_weight)
+        {
+            outputFile << "student weight should be between 0 and 260 !"<<endl;
+            inputFile.clear();
+            continue;
         }
 
         while( !eebuilding.isFull() )
         {
-            while(!train_station.isFull() && !student_weight.isEmpty())
-            {
-                int weight = student_weight.pop();
-                train_station.push(weight);
-            }
-
-            while(!train_station.isEmpty() && !bus.isFull())
-            {
-                int weight = train_station.pop();
-                bus.push(weight);
-                int bus_sum_weight = bus.sum();
-                int attempt = 0;
-                int train_station_size = train_station.size();
-                // 公車超重
-                while (bus_sum_weight > MAX_WEIGHT && attempt < train_station_size) {
-                    int bus_last_overweight = bus.pop();
-                    int bus_next_weight = train_station.pop();
-                    bus.push(bus_next_weight);
-                    train_station.push(bus_last_overweight);
-                    ++attempt;
-                    bus_sum_weight = bus.sum(); //迴圈內宣告只在迴圈內有效 所以這裡不能是int bus(後略
-                }
-                if (bus_sum_weight > MAX_WEIGHT) { 
-                    break;
-                }                  
-            }
-            while (!bus.isEmpty()) 
-            {
-                int weight_from_bus = bus.pop();
-                eebuilding.push(weight_from_bus);
-            }
-
+            student_to_station(train_station,student_weight);
+            station_to_bus(train_station,bus);
+            bus_to_eebuilding(bus,eebuilding);
         }
 
-        while( !eebuilding.isEmpty() ) 
-        {
-            int weight_eebuilding = eebuilding.pop();
-            outputFile << weight_eebuilding;
-
-            if (!eebuilding.isEmpty()) 
-            {
-                outputFile << ",";
-            }
-        }
+        eebuilding_print(eebuilding,outputFile);
 
         if (i != test_case - 1) 
         {
             outputFile << endl;
         }
-
     }
-
-
     inputFile.close();
     outputFile.close();
-    
     return 0;
+}
+
+void student_to_station(Queue& train_station, Queue& student_weight)
+{
+    while( !train_station.isFull() && !student_weight.isEmpty() ) 
+    {
+        int weight = student_weight.pop();
+        train_station.push(weight);
+    }
+}
+
+void station_to_bus(Queue& train_station, Stack& bus)
+{
+    while( !train_station.isEmpty() && !bus.isFull() ) 
+    {
+        int weight = train_station.pop();
+        bus.push(weight);
+        int bus_sum_weight = bus.sum();
+        int attempt = 0;
+        int train_station_size = train_station.size();
+        while (bus_sum_weight > MAX_WEIGHT && attempt < train_station_size) 
+        {
+            int bus_last_overweight = bus.pop();
+            train_station.push(bus_last_overweight);
+            int bus_next_weight = train_station.pop();
+            bus.push(bus_next_weight);
+            ++attempt;
+            bus_sum_weight = bus.sum();
+        }
+        if (bus_sum_weight > MAX_WEIGHT) 
+        { 
+            break;
+        }                  
+    }
+}
+
+void bus_to_eebuilding(Stack& bus, Queue& eebuilding)
+{
+    while( !bus.isEmpty() ) 
+    {
+        int weight = bus.pop();
+        eebuilding.push(weight);
+    }
+}
+
+void eebuilding_print(Queue& eebuilding, ofstream& outputFile)
+{
+    while( !eebuilding.isEmpty() ) 
+    {
+        int weight_eebuilding = eebuilding.pop();
+        outputFile << weight_eebuilding;
+        if (!eebuilding.isEmpty()) 
+        {
+            outputFile << ",";
+        }
+    }
 }
